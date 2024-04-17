f415593fb9b2cc75cb6bfffd2792fda5cb718ac2
Khawar Khan <ma3533899@gmail.com>
2024-04-17 14:45:15 +0500

    Added example routes

--- product.router.js
+++ product.router.js
@@ -1,7 +1,9 @@
 const Router = require('express').Router()
 const { getAllProducts, getProductById, postAddProduct } = require('./product.controller.js');
-
+//GET http://localhost:8000/product/all
 Router.get("/all", getAllProducts);
+//GET http://localhost:8000/product/1
 Router.get("/:id", getProductById);
+//POST http://localhost:8000/product/add
 Router.post("/add", postAddProduct);
 module.exports = Router;
